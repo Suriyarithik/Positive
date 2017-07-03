2081a2f6e8b68ca060999ffd1fa6b046bfebbf74
Suriyarithik <suriyavedsum@gmail.com>
2017-07-03 22:04:10 +0530

    Create Positive.java

---
+++ Positive.java
@@ -0,0 +1,19 @@
+# Positive
+import java.io.*;
+import java.util.*;
+public class Alpha
+{
+  public static void main(String args[])
+  {
+    Scanner sc=new Scanner(System.in);
+    int n=sc.nextInt();
+    if(n>0)
+    {
+      System.out.println(n+"is a POSITIVE number");
+    }
+    else
+    {
+      System.out.println(n+"is a NEGATIVE number");
+    }
+  }
+}  
